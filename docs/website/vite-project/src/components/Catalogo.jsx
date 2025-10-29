58584c72005d5431d3d5aa2aff3224948955d479
AlejandroLilloRodriguez <alejandrolillo.rodriguez@usp.ceu.es>
2025-10-29 13:08:34 +0100

    buscador funcionando

--- docs/website/vite-project/src/components/Catalogo.jsx
+++ docs/website/vite-project/src/components/Catalogo.jsx
@@ -1,20 +1,35 @@
+import { useState } from 'react';
 import data from '../data/contenidoCartas.json';
 import Card from './Card';
+import Buscador from './botones/BotonBuscador.jsx';
 
 function Catalogo() {
+    const [mostrarBuscador, setMostrarBuscador] = useState(false);
+    const [filtrar,setFiltrar] = useState("");
+
+    const cartasFiltradas = data.cartas.filter((carta)=>carta.titulo.toLowerCase().includes(filtrar.toLowerCase()));
   return (
     <div className="p-8">
       <div className="flex items-center justify-between mb-6">
         <h1 className="text-3xl font-bold text-gray-900 dark:text-white">
           Catálogo de Productos
         </h1>
-        <button className='border-indigo-600 bg-indigo-600 text-indigo-50 hover:bg-indigo-700 hover:border-indigo-700 hover:shadow-lg px-8 py-3 border-2 font-medium rounded-2xl transition-all duration-300 cursor-pointer'>
-          Filtrar Productos
-        </button>
+        <Buscador onfiltrar = {()=>setMostrarBuscador(!mostrarBuscador)}/>
       </div>
+      {mostrarBuscador && (
+        <div className="mb-6">
+          <input
+            type="text"
+            placeholder="Buscar producto..."
+            value={filtrar}
+            onChange={(e) => setFiltrar(e.target.value)}
+            className="w-full p-3 border border-gray-300 rounded-xl shadow-sm focus:ring-2 focus:ring-indigo-500 focus:outline-none dark:bg-gray-800 dark:text-white"
+          />
+        </div>
+      )}
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {data.cartas.map((carta, index) => (
+        {cartasFiltradas.map((carta, index) => (
           <Card
             key={index}
             titulo={carta.titulo}
