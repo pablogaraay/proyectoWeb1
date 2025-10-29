2e53dcfda81836745f5b1c3f160bd5a3d6e965d2
AlejandroLilloRodriguez <alejandrolillo.rodriguez@usp.ceu.es>
2025-10-29 12:03:46 +0100

    boton mejorado

--- docs/website/vite-project/src/components/Catalogo.jsx
+++ docs/website/vite-project/src/components/Catalogo.jsx
@@ -8,7 +8,7 @@ function Catalogo() {
         <h1 className="text-3xl font-bold text-gray-900 dark:text-white">
           Catálogo de Productos
         </h1>
-        <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors duration-200">
+        <button className='border-indigo-600 bg-indigo-600 text-indigo-50 hover:bg-indigo-700 hover:border-indigo-700 hover:shadow-lg px-8 py-3 border-2 font-medium rounded-2xl transition-all duration-300 cursor-pointer'>
           Filtrar Productos
         </button>
       </div>
