da5a82bfc7b285d8d7f500fc1285712a603ea172
Victor Vega Martinez <victor.vega.martinez09@gmail.com>
2025-10-30 10:52:06 +0100

    fix: add padding to the icon column in AboutUs component

--- docs/website/vite-project/src/components/AboutUs.jsx
+++ docs/website/vite-project/src/components/AboutUs.jsx
@@ -7,7 +7,7 @@ function AboutUs() {
     <div className='pl-8 pr-16 pt-10'>
       <div className='flex gap-25'>
         {/* Columna izquierda - Iconos */}
-        <div className='flex flex-col gap-18 '>
+        <div className='flex flex-col gap-18 pr-16 '>
           <RelojAboutUsIcono />
           <EstrellaAboutUsIcono />
         </div>
